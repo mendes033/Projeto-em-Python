e17028156e06da17b7c21d27add3139b2d8cad75
mendes033 <joaovitorsousamendes@gmail.com>
2023-05-19 00:23:04 -0300

    Consegui pegar os diabo dos valores

--- Code.py
+++ Code.py
@@ -16,6 +16,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
 
 
+class Produto:
+
+    def __init__(self, nome, valor_real, valor_promo, temp_promo, link):
+
+        self.nome = nome
+        self.valor_real = valor_real
+        self.valor_promo = valor_promo
+        self.temp_promo = temp_promo
+        self.link = link
 
 load_dotenv()
 USER_LOGIN = os.getenv('LOGIN')
@@ -30,80 +39,39 @@ service = Service(executable_path="/usr/local/bin/chromedriver")
 with webdriver.Chrome(service=service) as driver:
     driver.get("https://estudante.estacio.br/")
     driver.implicitly_wait(5)
-
     driver.maximize_window()
-
-    # driver.get("https://estudante.estacio.br/")
-    # time.sleep(5)
-    # bt0 = driver.find_element_by_xpath("/html/body/div[1]/section/div/div/div/section/div[1]/button").click()
-    # time.sleep(15)
 
     driver.find_element(By.XPATH, "/html/body/div[1]/section/div/div/div/section/div[1]/button").click()
     time.sleep(5)
 
-    # username_textbox=driver.find_element_by_name("loginfmt")
-    # username_textbox.send_keys(user)
-
     username_textbox=driver.find_element(By.NAME, "loginfmt")
     username_textbox.send_keys(user)
-
-    # bt1 = driver.find_element_by_id("idSIButton9").click()
-    # time.sleep(5)
 
     driver.find_element(By.ID , "idSIButton9").click()
     time.sleep(5)
 
-    # password=driver.find_element_by_id("i0118")
-    # password.send_keys(password_user)
-
     password=driver.find_element(By.ID , "i0118")
     password.send_keys(password_user)
-
-
-    # login_attempt = driver.find_element_by_id("idSIButton9")
-    # login_attempt.submit()
-    # time.sleep(5)
 
     login_attempt = driver.find_element(By.ID , "idSIButton9")
     login_attempt.submit()
     time.sleep(5)
 
-    # bt2 = driver.find_element_by_id("idBtn_Back").click()
-    # time.sleep(5)
-
     driver.find_element(By.ID , "idBtn_Back").click()
     time.sleep(5)
 
-    # div/div/div/article/div/div[1]/section/header/button").click()
     driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/section/article/section[1]/section/div[2]/div/div/section[2]/header/button").click()
     time.sleep(5)
-
-
-    # bt4 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/section/div/div/div[1]/section/div/div/article/section[3]/div[2]/div[3]/div/section/div[5]/a").click()
-    # time.sleep(5)
 
     driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/section/aside/section/section[2]/div[1]/button[2]").click()
     time.sleep(5)
 
-    # bt5 = driver.find_element_by_xpath("/html/body/div[1]/section/article[2]/article[2]/section/div/div/div[2]/section/div/div/article/section/section/section/div[3]/div[2]/div/section/div/div[1]/button").click()
-    # time.sleep(5)
-
     driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/section/aside/section/section[2]/div[3]/section[2]/section/div[3]/div[2]/div/section/div/div[1]/button").click()
     time.sleep(5)
 
-    # driver.get("https://www.kabum.com.br/")
-    # driver.switch_to_window(driver.window_handles[1])
     driver.switch_to.window(driver.window_handles[1])
     driver.get(driver.current_url)
     time.sleep(5)
-
-    # driver.switch_to.new_window("https://www.kabum.com.br/")
-    # time.sleep(5)
-    # val = "https://www.kabum.com.br/"
-    # wait = WebDriverWait(driver, 5)
-    # driver.get(val)
-    # wait.until(EC.url_to_be(val))
-    # page_source = driver.page_source
 
     # produto = str(input("Digite o nome do produto: "))
     produto = "Fone HyperX"
@@ -114,18 +82,31 @@ with webdriver.Chrome(service=service) as driver:
     busca_attempt = driver.find_element(By.XPATH, "/html/body/div[1]/header/div[1]/div/div/div[1]/div[3]/div/form/button").click()
     time.sleep(5)
 
+    lista_produtos = []
+    i = 0
+    nome_produto = ""
 
-class Produto:
+    while i>=0:
+        i = i + 1
+        path_nome = "/html/body/div[1]/div[1]/div[3]/div/div/div[2]/div/main/div["+str(i)+"]/a/div/button/div/h2/span"
+        path_valor_real = "/html/body/div[1]/div[1]/div[3]/div/div/div[2]/div/main/div["+str(i)+"]/a/div/div[1]/span[1]"
+        path_valor_promo = "/html/body/div[1]/div[1]/div[3]/div/div/div[2]/div/main/div["+str(i)+"]/a/div/div[1]/span[2]"
+        # path_temp_promo = "/html/body/div[1]/div[1]/div[3]/div/div/div[2]/div/main/div["+str(i)+"]/div[2]/div[2]/div/div[2]/div/span"
+        # path_link = "/html/body/div[1]/div[1]/div[3]/div/div/div[2]/div/main/div["+str(i)+"]/a"
 
-    def __init__(self, nome, idade):
+        nome_produto = driver.find_elements(By.XPATH, path_nome)
+        valor_real = driver.find_elements(By.XPATH, path_valor_real)
+        valor_promo = driver.find_elements(By.XPATH, path_valor_promo)
+        # temp_promo = driver.find_elements(By.XPATH, path_temp_promo)
+        # link = driver.find_elements(By.XPATH, path_link)
 
-        self.nome = nome
+        lista_produtos.append(Produto(nome_produto[0].text, valor_real[0].text, valor_promo[0].text, "", ""))
 
-        self.idade = idade
 
-    def falar(self):
 
-        print(f"O {self.nome} está falando.")
+
+
+
 
 # driver.quit()
 
